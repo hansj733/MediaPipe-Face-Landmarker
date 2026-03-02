ed11267a15d539f5d8970b5a78a969ddacec7e63
hansj <jkhhkhh8@gmail.com>
2026-03-02 23:26:36 +0900

    CSS import

--- src/main.js
+++ src/main.js
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+import './style.css';
 import * as vision from '@mediapipe/tasks-vision';
 console.log('JS FILE LOADED');
 const { FaceLandmarker, FilesetResolver, DrawingUtils } = vision;
